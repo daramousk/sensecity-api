770e83ca49916e094ed96c0c367f31952f7214d4
kbakoulias <kostas.bakoulias@gmail.com>
2016-02-17 13:10:42 +0200

    Change issues.js database Schema

--- models/issue.js
+++ models/issue.js
@@ -7,7 +7,8 @@ var issueSchema = new mongoose.Schema({
 	loc :  { type: {type:String}, coordinates: [Number]},
 	issue: String,
 	create_at: {type: Date, default: Date.now},
-  device_id: String 
+	device_id: String,
+	value_desc: String
 });
 
 issueSchema.index({loc: "2dsphere"});

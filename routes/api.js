83689a52a3913fa89855253e3b44b87d1651b1b3
kbakoulias <kostas.bakoulias@gmail.com>
2016-03-31 17:31:43 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -115,14 +115,17 @@ router.get('/issue', function(req, res){
    //db.issues.find({create_at:{$gt: new ISODate('2016-01-22T00:00:00:000Z'), $lt: new ISODate('2016-03-31T00:00:00:000Z')}}).sort({create_at:-1}).limit(5)
 
    
+   
   if(_coordinates === ''){
 	  if( _issue === '')
 	  {
-		  console.log('_coordinates null 1 '+_startdate+' - '+_enddate+' - '+_sort+' - '+_limit);
+		  var xxxx= new Date(_startdate);
+		  var yyyy= new Date(_enddate);
+		  console.log('_coordinates null 1 '+xxxx+' - '+yyyy+' - '+_sort+' - '+_limit);
 		  //http://api.sense.city:3005/api/issue?startdate=2016-01-22T00:00:00:000Z&enddate=2016-03-28T00:00:00:000Z&coordinates=[21.734574,38.2466395]&distance=1000&issue=garbage
 		  //Issue.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 		  //{create_at:{$gte:_startdate, $lt:_enddate}}
-		  Issue.find({create_at:{$gte:_startdate, $lt:_enddate}},function(err, issue){
+		  Issue.find({"create_at":{$gte:xxxx, $lt:yyyy}},function(err, issue){
 			res.send(err);
 		  });//.sort({create_at:_sort}).limit(_limit);
 	  }

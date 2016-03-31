0c816c9ec315d919194996a14ea02b1fc4014b20
kbakoulias <kostas.bakoulias@gmail.com>
2016-03-31 17:21:27 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -123,7 +123,7 @@ router.get('/issue', function(req, res){
 		  //Issue.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 		  //{create_at:{$gte:_startdate, $lt:_enddate}}
 		  Issue.find(function(err, issue){
-			res.send(err);
+			res.send(issue);
 		  });//.sort({create_at:_sort}).limit(_limit);
 	  }
 	  else{

4bcae3a54d56fbe50536a3ede3658585c7fdef8d
kbakoulias <kostas.bakoulias@gmail.com>
2016-03-31 15:54:39 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -113,7 +113,7 @@ router.get('/issue', function(req, res){
 		  //http://api.sense.city:3005/api/issue?startdate=2016-01-22T00:00:00:000Z&enddate=2016-03-28T00:00:00:000Z&coordinates=[21.734574,38.2466395]&distance=1000&issue=garbage
 		  //Issue.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 		  //{create_at:{$gte:_startdate, $lt:_enddate}}
-		  Issue.find({create_at:{$gte:'2016-2-28T00:00:00:000Z', $lt:'2016-2-31T23:59:59:000Z'}
+		  Issue.find({"create_at":{"$gte":'2016-2-28T00:00:00:000Z', "$lt":'2016-2-31T23:59:59:000Z'}
 							},function(err, issue){
 			res.send(issue);
 		  });//.sort({create_at:_sort}).limit(_limit);

8da36e40cdb6534c8b6eff303b9216dd869770c7
kbakoulias <kostas.bakoulias@gmail.com>
2016-04-11 18:18:05 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -110,18 +110,18 @@ router.get('/issue', function(req, res){
 	if(_coordinates === ''){
 	  if( _issue === '')
 	  {
-		  Issue.find({"create_at":{$gte:_startdate, $lt:_enddate}},{"image_name":_image},function(err, issue){
+		  Issue.find({"create_at":{$gte:_startdate, $lt:_enddate}},function(err, issue){
 			res.send(issue);
-		  }).sort({create_at:_sort}).limit(_limit);
+		  },{"image_name":_image}).sort({create_at:_sort}).limit(_limit);
 	  }
 	  else{
 		  
 		//Issue.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 		Issue.find({"create_at":{$gte:_startdate, $lt:_enddate},
 							 "issue":_issue
-							},{"image_name":_image}, function(err, issue){
+							}, function(err, issue){
 			res.send(issue);
-		  }).sort({"create_at":_sort}).limit(_limit);
+		  },{"image_name":_image}).sort({"create_at":_sort}).limit(_limit);
 		  
 	  }	  
 	}
@@ -132,16 +132,16 @@ router.get('/issue', function(req, res){
 			//http://api.sense.city:3005/api/issue?startdate=2016-01-22T00:00:00:000Z&enddate=2016-03-28T00:00:00:000Z&coordinates=[21.734574,38.2466395]&distance=1000&issue=garbage
 			Issue.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 					"create_at":{$gte:_startdate, $lt:_enddate}
-				},{"image_name":_image}, function(err, issue){
+				}, function(err, issue){
 				res.send(issue);
-			}).sort({"create_at":_sort}).limit(_limit);
+			},{"image_name":_image}).sort({"create_at":_sort}).limit(_limit);
 		}
 		else{			
 			Issue.find({"issue":_issue,"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 					"create_at":{$gte:_startdate, $lt:_enddate}							 
-				},{"image_name":_image}, function(err, issue){
+				}, function(err, issue){
 					res.send(issue);
-				}).sort({"create_at":_sort}).limit(_limit);
+				},{"image_name":_image}).sort({"create_at":_sort}).limit(_limit);
 		}
 	}
 	

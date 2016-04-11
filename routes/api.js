4be970ef2a3b17944463f8eba374aeb51880a5ef
kbakoulias <kostas.bakoulias@gmail.com>
2016-04-11 18:11:44 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -98,12 +98,11 @@ router.get('/issue', function(req, res){
 	}
 	if (!req.query.hasOwnProperty('image_field'))
 	{
-		_image ='';
+		_image =1;
 	}
 	else{
-		console.log("image = "+req.query.image_field);
-		console.log("--------------------------------");
-		_image = {"image_name:"req.query.image_field};
+		
+		_image = req.query.image_field;
 		console.log(_image);
 	}
 	
@@ -111,14 +110,14 @@ router.get('/issue', function(req, res){
 	if(_coordinates === ''){
 	  if( _issue === '')
 	  {
-		  Issue.find({},_image,{"create_at":{$gte:_startdate, $lt:_enddate}},function(err, issue){
+		  Issue.find({},{"image_name":_image},{"create_at":{$gte:_startdate, $lt:_enddate}},function(err, issue){
 			res.send(issue);
 		  }).sort({create_at:_sort}).limit(_limit);
 	  }
 	  else{
 		  
 		//Issue.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
-		Issue.find({},_image,{"create_at":{$gte:_startdate, $lt:_enddate},
+		Issue.find({},{"image_name":_image},{"create_at":{$gte:_startdate, $lt:_enddate},
 							 "issue":_issue
 							}, function(err, issue){
 			res.send(issue);
@@ -131,14 +130,14 @@ router.get('/issue', function(req, res){
 		if(_issue === '')
 		{
 			//http://api.sense.city:3005/api/issue?startdate=2016-01-22T00:00:00:000Z&enddate=2016-03-28T00:00:00:000Z&coordinates=[21.734574,38.2466395]&distance=1000&issue=garbage
-			Issue.find({},_image,{"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
+			Issue.find({},{"image_name":_image},{"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 					"create_at":{$gte:_startdate, $lt:_enddate}
 				}, function(err, issue){
 				res.send(issue);
 			}).sort({"create_at":_sort}).limit(_limit);
 		}
 		else{			
-			Issue.find({},_image,{"issue":_issue,"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
+			Issue.find({},{"image_name":_image},{"issue":_issue,"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:JSON.parse(req.query.coordinates)},$maxDistance:JSON.parse(req.query.distance)}},
 					"create_at":{$gte:_startdate, $lt:_enddate}							 
 				}, function(err, issue){
 					res.send(issue);

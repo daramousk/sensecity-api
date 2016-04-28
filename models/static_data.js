200cb478710ecaeba3f05de9b1e7b26bdf9063e8
kbakoulias <kostas.bakoulias@gmail.com>
2016-04-28 17:16:17 +0300

    ok

---
+++ models/static_data.js
@@ -0,0 +1,23 @@
+// Dependencies
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+// Schema
+var static_dataSchema = new mongoose.Schema({
+	municipality:String,
+  type:String,
+  subtype:String,
+  dimos_item_id:String,
+  loc:{
+		type: String,
+		coordinates:[Number], //[<longitude>,<lattitude>]
+	},
+	notes: String,
+	create_at: Date
+});
+
+static_dataSchema.index({loc: "2dsphere"});
+
+// Return model
+module.exports = restful.model('static_data', static_dataSchema);
+
---
+++ routes/static_data.js
@@ -0,0 +1,29 @@
+
+var express = require('express');
+var static_router = express.Router();
+var mongoose = require('mongoose');
+var fs = require('fs');
+
+console.log("asdfghjkl");
+
+//mongoose.connect('mongodb://localhost/sensecity');
+
+// Models
+
+var Static_data = require('../models/static_data');
+
+// Routes
+/*
+Static_data.methods(['get', 'put', 'post', 'delete']);
+Static_data.register(static_router,'/static_data');
+*/
+static_router.get('/static_data', function(req, res){		
+		console.log("get");
+}); 
+
+static_router.post('/static_data', function(req, res){		
+		console.log("post");
+}); 
+
+// Return router
+module.exports = static_router;

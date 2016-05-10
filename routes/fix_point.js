a1236322236b8efe42e2f2375a8068c88e12e7b6
kbakoulias <kostas.bakoulias@gmail.com>
2016-05-10 16:13:36 +0300

    ok

---
+++ routes/fix_point.js
@@ -0,0 +1,36 @@
+
+var express = require('express');
+var static_router = express.Router();
+var mongoose = require('mongoose');
+var fs = require('fs');
+
+
+// Models
+
+var static_data = require('../models/fix_point');
+
+
+
+static_router.get('/:long/:lat/:dist/data', function(req, res){		
+	static_data.find({Dimos:req.params.city}, function(err, issue){
+		res.send(issue);
+  });
+}); 
+
+static_router.get('/:long/:lat/:dist/garbage', function(req, res){		
+	console.log("garbage");
+	static_data.find({Dimos:req.params.city,type:"garbage"},function(err, issue){
+		res.send(issue);
+  });
+}); 
+
+static_router.get('/:long/:lat/:dist/fotistiko', function(req, res){		
+	static_data.find({Dimos:req.params.city,type:"fotistiko"}, function(err, issue){
+		res.send(issue);
+  });
+}); 
+
+
+
+// Return router
+module.exports = static_router;

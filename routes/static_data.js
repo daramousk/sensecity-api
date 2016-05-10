e8d5d32fe62fbf0160063de03946eb7a8815929c
kbakoulias <kostas.bakoulias@gmail.com>
2016-05-10 15:20:47 +0300

    ok

--- routes/static_data.js
+++ routes/static_data.js
@@ -19,9 +19,12 @@ static_router.get('/data', function(req, res){
 
 static_router.get('/garbage', function(req, res){		
 	console.log("garbage");
-	static_data.find({type:"garbage"}, function(err, issue){
+	mongoose.model('satic_data').find({}, function(err, data){
+		res.send(data);
+	});
+	/*static_data.find({type:"garbage"}, function(err, issue){
 		res.send(issue);
-  });
+  });*/
 }); 
 
 static_router.get('/fotistiko', function(req, res){		

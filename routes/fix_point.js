eba4c7499ac8ea6db8e35826ef7a2dd643837ec1
kbakoulias <kostas.bakoulias@gmail.com>
2016-05-10 16:37:57 +0300

    ok

--- routes/fix_point.js
+++ routes/fix_point.js
@@ -12,20 +12,23 @@ var static_data = require('../models/fix_point');
 
 
 static_router.get('/:long/:lat/:dist/data', function(req, res){		
-	static_data.find(function(err, issue){
+	static_data.find("loc":{$nearSphere:{$geometry:{type:"Point",coordinates:[req.params._long,req.params._lat]},$maxDistance:req.params._dist}}
+			},function(err, issue){
 		res.send(issue);
   });
 }); 
 
-static_router.get('/:long/:lat/:dist/garbage', function(req, res){		
+static_router.get('/:_long/:_lat/:_dist/garbage', function(req, res){		
 	console.log("garbage");
-	static_data.find(function(err, issue){
+	static_data.find({"loc":{$nearSphere:{$geometry:{type:"Point",coordinates:[req.params._long,req.params._lat]},$maxDistance:req.params._dist}}
+			},function(err, issue){
 		res.send(issue);
   });
 }); 
 
 static_router.get('/:long/:lat/:dist/fotistiko', function(req, res){		
-	static_data.find(function(err, issue){
+	static_data.find("loc":{$nearSphere:{$geometry:{type:"Point",coordinates:[req.params._long,req.params._lat]},$maxDistance:req.params._dist}}
+			},function(err, issue){
 		res.send(issue);
   });
 }); 

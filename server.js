963dc2cd9e10ef46a690cbeb5adcdfb4728a9226
kbakoulias <kostas.bakoulias@gmail.com>
2016-05-18 13:04:23 +0300

    ok

--- server.js
+++ server.js
@@ -8,16 +8,8 @@ var bodyParser = require('body-parser');
 
 // Express
 var app = express();
-
+app.use(bodyParser.urlencoded({limit: '10mb'},{extended: true}));
 app.use(bodyParser.json({limit: '10mb'}));
-
-app.use(bodyParser.urlencoded({     
-        extended: true,
-        keepExtensions: true,
-        limit: '10mb', 
-        defer: true
-    }));
-
 
 
 //headers

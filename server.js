85a84fbed1d245e1e1c107447c00f774896eef09
kbakoulias <kostas.bakoulias@gmail.com>
2016-05-18 12:57:45 +0300

    ok

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(function (req, res, next) {
 });
 
 // Routes
-app.use('/api', express.bodyParser({keepExtensions: true, limit: 1024 * 1024 * 1024 * 500, defer: true }, require('./routes/api'));
+app.use('/api', express.bodyParser({keepExtensions: true, limit: 53000000000, defer: true }, require('./routes/api'));
 
 app.use('/fixed-point',require('./routes/lighting'));
 

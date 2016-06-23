7154d62ac171653cd37735b84937aadfd9cbf7b7
kbakoulias <kostas.bakoulias@gmail.com>
2016-06-23 17:35:11 +0300

    ok

--- routes/fix_point.js
+++ routes/fix_point.js
@@ -13,7 +13,7 @@ var static_data = require('../models/fix_point');
 
 static_router.get('/:_long/:_lat/:_dist/data', function(req, res){		
 	static_data.find({ loc:{$nearSphere:{$geometry:{type:"Point",coordinates:[parseFloat(req.params._long),parseFloat(req.params._lat)]},$maxDistance:parseFloat(req.params._dist)}}
-			}, { "loc.coordinates": 1, type: 1, notes: 1, _id: 0 } ,function(err, issue){
+			}, { "loc.coordinates": 1, type: 1, "notes.ANAKIKLOSI": 1, _id: 0 } ,function(err, issue){
 		res.send(issue);
   });
 }); 

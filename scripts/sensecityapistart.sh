78cec1a7e9dde82b112341c74adfb48237c45e9d
kbakoulias <kostas.bakoulias@gmail.com>
2016-07-29 10:29:56 +0300

    ok

--- scripts/sensecityapistart.sh
+++ scripts/sensecityapistart.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 cd /opt/sensecity-api/;node /opt/sensecity-api/server.js >> /var/log/sensecityapi.log 2>&1 &
-node /opt/bugzillaAPI/server.js >> /var/log/sensecity_bugzillaAPI.log 2>&1cd ../
+cd /opt/bugzillaAPI/;node server.js >> /var/log/sensecity_bugzillaAPI.log 2>&1cd ../

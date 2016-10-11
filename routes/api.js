61d2cbc6c7f9b854b03ebfbf837c18a6d8f1724f
kbakoulias <kostas.bakoulias@gmail.com>
2016-10-11 13:10:58 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -558,8 +558,8 @@ router.get('/issue_test', function(req, res) {
 		json: bugParams
 	}, function (error, response, body) {
 			
-			console.log("response ====> "+JSON.parse(JSON.stringify(response)));
-			console.log("body ==========>"+JSON.parse(JSON.stringify(body)));
+			console.log("response ====> "+JSON.parse(JSON.stringify(response.statusCode)));
+			console.log("body ==========>"+JSON.parse(JSON.stringify(body.result)));
 			
 			/*if (!error && response.statusCode === 200) {
 							bugToken = body.result.token;

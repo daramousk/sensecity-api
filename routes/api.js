e710e94180c0175df9785ef17cfa1448de5c4c57
kbakoulias <kostas.bakoulias@gmail.com>
2016-10-12 17:06:53 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -571,7 +571,7 @@ router.get('/issue_test', function(req, res) {
 		for(i_count=0;i_count<body.result.bugs.length;i_count++)
 		{
 			//console.log(i_count + "<=====>" + body.result.bugs[i_count].alias);
-			ids += String(body.result.bugs[i_count].alias);
+			ids += '"'+body.result.bugs[i_count].alias+'"';
 			if(i_count<body.result.bugs.length-1)
 			{
 				ids +=' , ';

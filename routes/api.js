d955bde0fdd66019bf02805b1445f26725090edc
kbakoulias <kostas.bakoulias@gmail.com>
2016-10-12 16:56:58 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -585,7 +585,7 @@ router.get('/issue_test', function(req, res) {
 		console.log("-------------------------");
 		console.log("=========================");
 		//Issue.find({'_id': {$in: [ids]}} , function(err, issue){
-		Issue.find({"_id": [ "57fe2f6659ed960e3c0cb850" , "57fe296459ed960e3c0cb84f" , "57fe235c59ed960e3c0cb84e"]} , function(err, issue){
+		Issue.find({"_id": [ 57fe2f6659ed960e3c0cb850 , 57fe296459ed960e3c0cb84f , 57fe235c59ed960e3c0cb84e]} , function(err, issue){
 				
 				console.log("err   =   "+err);
 				console.log(issue);

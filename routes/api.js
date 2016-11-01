ae4db990998abc6b1f9f8a19ad620b5ece6085ea
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-01 12:47:27 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -141,7 +141,7 @@ router.post('/issue', function (req,res){
 									}
 							}
 							console.log('saved: ', resp);
-							res.send([resp,{"test","test1"}]);
+							res.send([resp,{"test":"test1"}]);
 						}
 					});
 				});

22303b69baaf19ad2ae116bb62f99043ccfd9237
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-02 17:07:20 +0200

    ok

--- models/active_user.js
+++ models/active_user.js
@@ -1,15 +1,16 @@
 // Dependencies
 var restful = require('node-restful');
 var mongoose = restful.mongoose;
-
+	
 // Schema
 var active_userSchema = new mongoose.Schema({
 	uuid :  String,
-	name: String,	
-	email: String,
-	mobile_num: {type:String, default : ""},	
-	create_at: {type: Date, default: Date.now},
-	permission :  { send_issues: {type:String, default : "false"}, communicate_with: {email : {type:String, default : "false"}, sms : {type:String, default : "false"}}}
+	name : String,	
+	email : String,
+	mobile_num : {type:String, default : ""},	
+	create_at : {type: Date, default: Date.now},
+	permission :  { send_issues: {type:String, default : "false"}, communicate_with: {email : {type:String, default : "false"}, sms : {type:String, default : "false"}}},
+	activate : {type:String, default : ""}
 });
 
 // Return model

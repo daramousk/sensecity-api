3272b3fd1ed0b243da582b999677464148a74f44
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-18 11:04:06 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -844,7 +844,7 @@ router.get('/issue_test', function(req, res) {
 
 				
   
-					
+					console.log("length=====" + bugzilla_results.length);
 					for(var j=0;j<bugzilla_results.length;j++){
 						if(bugzilla_results[j].alias[0]===issue[i]._id){
 							console.log("id========>"+bugzilla_results[j].id);

a9e52e11e7bc1e8a9920c538f7332c5e25c6a9c6
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-21 11:57:08 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -1918,7 +1918,7 @@ router.get('/fullissue/:id', function(req, res){
 			json: bugParams
 		}, function (error1, response1, body1) {
 			
-			console.log("comment =======> : "+JSON.stringify(body1, null, 2) );
+			console.log("comment =======> : "+body1 );
 			console.log("comments ==============>>>>>>>>"+response1);
 		
 			Issue.findOne({"_id":req.params.id},function(err, issue){

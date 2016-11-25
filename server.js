76c75a6fddd46620d8443544476ef09103da992a
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-25 10:07:51 +0200

    ok

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
+var crypto = require('crypto-js');
+var request = require('request');
 
 // Mongo Db
 //mongoose.connect('mongodb://localhost/sensecity');
@@ -16,7 +17,7 @@ var config = require('app-config');
 
 //headers
 app.use(function (req, res, next) {
- 
+
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -24,7 +25,9 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,X-Requested-With,content-type,x-uuid,x-role');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length,x-uuid,Cookie,Set-Cookie,Host,Accept,User-Agent,x-role');
+	res.header('Access-Control-Expose-Headers', 'Content-Type,Content-Length,x-uuid,Cookie,Set-Cookie,Host,Accept,User-Agent,x-role');
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
@@ -34,19 +37,124 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Routes
-app.use('/api', require('./routes/api'));
+var bugUrl = config.config.bugUrl;
 
-app.use('/fixed-point',require('./routes/lighting'));
 
-app.use('/api/issue',require('./routes/image_return'));
+function authorization(req, res, next) {
+   Role.find({uuid: req.get('x-uuid')}, function (err, response) {
+        if (response.length > 0 && response[0]["timestamp"] >= Date.now()) {
+            if (req.path === '/admin/bugs/search') {
+                if (req.get('x-role') === 'departmentAdmin' || req.get('x-role') === 'sensecityAdmin' || req.get('x-role') === 'departmentUser' || req.get('x-role') === 'cityAdmin') {
+                    next();
+                } else {
+                    res.send("failure");
+                }
+            }
+        } else {
+            res.send("failure");
+        }
+   });
 
-app.use('/fix_point', require('./routes/fix_point'));
+}
 
-app.use('/mobilemap', require('./routes/api'));
 
-// start server
-app.listen(config.config.port);
-console.log('API is running on port '+config.config.port);
-/*app.listen(config.config.port);
-console.log('API is running on port '+config.config.port);*/
+
+var Role = require('./models/roles.js');
+  
+function authentication(req,res,next) {
+	Role.find({uuid: req.get('x-uuid')}, function(err, response) {
+		if (response.length > 0 && response[0]["timestamp"] >= Date.now()) {
+			next();
+		} else { 
+			 res.send("failure");  
+		}
+	});
+}
+
+
+
+mongoose.connection.once('open', function () {
+	app.post('/dashboard', function (req, res) { 
+	Role.find({username: req.body.username, password: req.body.password}, function (err, response) { 
+		if (response.length > 0) { 
+			var wordArray = crypto.enc.Utf8.parse(req.body.username, req.body.password); 
+			var uuid = crypto.enc.Base64.stringify(wordArray); app.use('/mobilemap', require('./routes/api'));
+			Role.update({username: req.body.username, password: req.body.password}, {$set: {"uuid": uuid, "timestamp": Date.now() * 1000 * 3600}}, {multi: true}, function (err, doc) {});
+			return res.send(response[0]["city"] + ";" + response[0]["role"] + ";" + response[0]["department"] + ";" + response[0]["email"] + ";" + uuid + ";" + req.body.username); 
+			} else {
+				return res.send("failure"); 
+			}
+		});
+	});
+	app.get('/get', authentication, function (req, res) {
+		res.send("success");
+	});
+	app.get('/logout', authentication, function (req, res) {
+		
+		console.log("fsdlghfksdlghfdlk");
+		console.log(req.get('x-uuid'));
+
+		Role.update({uuid: req.get('x-uuid')},{$unset: {"uuid": 1, "timestamp": 1}}, function(err, response) {
+			res.send("logout");
+		});
+	});
+
+                    app.post('/admin/bugs/search', authorization, function (req, res) {
+                        var bugToken = "";
+                        var loginData = {"method": "User.login", "params": [{"login": config.config.login, "password": config.config.pwd}],"id": 1 };
+                        request({
+                            url: bugUrl,
+                            method: "POST",
+                            json: loginData
+                        }, function (error, response, body) {
+                            if (!error && response.statusCode === 200) {
+                                bugToken = body.result.token;
+                                req.body.params[0].token = bugToken;
+                                request({
+                                    url: config.config.bugUrl,
+                                    method: "POST",
+                                    headers: {'content-type': 'application/json'},
+                                    json: req.body
+                                }, function (error, response, body) {
+                                    if (!error && response.statusCode === 200) {
+                                        console.log(req.body);
+                                        if (response.body.result !== null)
+                                        {
+                                            console.log("problem reported " + response.body.result.bugs.length);
+                                            res.send(response.body.result.bugs);
+                                        } else
+                                        {
+                                            console.log(response.body.error);
+                                            res.send([response.body.error]);
+                                        }
+                                    } else {
+                                        console.log("error: " + error);
+                                    }
+                                });
+                            } else {
+                                console.log("error: " + error);
+                               // console.log("response.statusCode: " + response.statusCode);
+                               // console.log("response.statusText: " + response.statusText);
+                            }
+                        });
+                    });
+
+});
+
+
+
+
+// Routes 
+app.use('/api/1.0', require('./routes/api'));
+ 
+app.use('/fixed-point',require('./routes/lighting')); 
+
+app.use('/api/1.0/issue',require('./routes/image_return')); 
+app.use('/fix_point', require('./routes/fix_point')); 
+app.use('/api/1.0/mobilemap', require('./routes/api')); 
+
+// start server 
+app.listen(config.config.port); 
+console.log('API is running on port '+config.config.port); 
+
+/*app.listen(config.config.port); console.log('API is running on port '+config.config.port);*/

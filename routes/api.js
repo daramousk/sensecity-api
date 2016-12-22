fbf04afe0290c7a055c9d68566fd668f88cdd825
kbakoulias <kostas.bakoulias@gmail.com>
2016-12-22 13:51:20 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -1681,13 +1681,13 @@ router.get('/issue/:city', function (req, res) {
     }
 	
 	if (!req.query.hasOwnProperty('includeAnonymous')){
-		_cf_authedicated = '"cf_authedicated":1';
+		_cf_authedicated = 1;
 	}
 	else{
 		if(req.query.includeAnonymous==1){
-			_cf_authedicated = '';
+			_cf_authedicated = [0,1];
 		}else{
-			_cf_authedicated = '"cf_authedicated":1';
+			_cf_authedicated = 1;
 		}
 		
 	}
@@ -1704,7 +1704,7 @@ router.get('/issue/:city', function (req, res) {
     var bugParams =
             {
                 "method": "Bug.search",
-                "params": [{"product": _product, "order": "bug_id DESC", "limit": _limit, "status": _status, "cf_issues": _issue, _cf_authedicated, "f1": "creation_ts", "o1": "greaterthan", "v1": "2016-01-01", "include_fields": ["id", "alias", "status"]}],
+                "params": [{"product": _product, "order": "bug_id DESC", "limit": _limit, "status": _status, "cf_issues": _issue, "cf_authedicateds":_cf_authedicated, "f1": "creation_ts", "o1": "greaterthan", "v1": "2016-01-01", "include_fields": ["id", "alias", "status"]}],
                 "id": 1
             };
 

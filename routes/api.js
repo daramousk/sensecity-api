540af3a1b162300fe4f160f63d2832f0b8670312
kbakoulias <kostas.bakoulias@gmail.com>
2016-12-24 16:33:34 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -1799,7 +1799,7 @@ router.get('/issue/:city', function (req, res) {
 		
 		console.log(JSON.stringify(body));
 		
-        if (body.length < 1 || body==null)
+        if (body==null || body.length < 1 || body==null )
         {
 			console.log("-1");
             res.send([{}]);

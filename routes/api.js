979a917a646b5dd8e2b79c7f3a64ea83cd7f8620
kbakoulias <kostas.bakoulias@gmail.com>
2017-01-10 12:32:25 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -32,9 +32,9 @@ function authorization(req, res, next) {
     Role.find({uuid: req.get('x-uuid')}, function (err, response) {
         if (response.length > 0 && response[0]["timestamp"] >= Date.now()) {
             if (req.path === '/admin/bugs/search' || req.path === '/admin/bugs/update' || req.path === '/admin/bugs/comment' || req.path === '/admin/bugs/comment/tags' || req.path === '/admin/bugs/comment/add') {
-				
+				console.log("x-role"+req.get('x-role'));
                 if (req.get('x-role') === 'departmentAdmin' || req.get('x-role') === 'sensecityAdmin' || req.get('x-role') === 'departmentUser' || req.get('x-role') === 'cityAdmin') {
-					
+					console.log("success");
                     next();
                 } else {
                     res.send("failure");

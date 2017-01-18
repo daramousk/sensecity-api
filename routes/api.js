3c5dcf0386b3db25e028e17911e6b01e2cc85fc1
kbakoulias <kostas.bakoulias@gmail.com>
2017-01-18 17:39:27 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -585,7 +585,7 @@ router.get('/issue', function (req, res) {
         method: "GET"
     }, function (error, response, body) {
 		
-		console.log(JSON.stringify(body));
+		console.log(JSON.stringify(response));
 		
 	});
 	

44541e8d85a5fbd77fbe02b1874d09ccd95f829b
kbakoulias <kostas.bakoulias@gmail.com>
2017-01-20 12:57:52 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -511,17 +511,17 @@ router.get('/issue', function (req, res) {
 		}
 		if (!req.query.hasOwnProperty('image_field'))
 		{
-			_image = ",image_name=false"
+			_image = ",image_name=false";
 			//_image = true;
 			//console.log("1 _image=" + _image);
 		} else {
 			if (req.query.image_field == 0)
 			{
-				_image = ",image_name=false"
+				_image = ",image_name=false";
 				//_image = false;
 				//console.log("2 _image=" + _image);
 			} else {
-				_image = ",image_name=true"
+				_image = ",image_name=true";
 				//_image = true;
 				//console.log("2 _image=" + _image);
 			}
@@ -554,7 +554,7 @@ router.get('/issue', function (req, res) {
 			
 			_product = req.query.city;
 		}
-		console.log(_product);
+		
 		if (!req.query.hasOwnProperty('status'))
 		{
 			_status = ["CONFIRMED", "IN_PROGRESS"];
@@ -645,7 +645,7 @@ router.get('/issue', function (req, res) {
 		
 		
 
-		Issue.find({'_id': {$in: ids}},{"user":_user+_image}, function (err, issue) {
+		Issue.find({'_id': {$in: ids}},{"user":_user + _image}, function (err, issue) {
 
                 //new start
                 if(err!=null){console.log("err   =   " + err);}

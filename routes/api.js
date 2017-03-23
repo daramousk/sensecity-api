1b468ddf5c87e83f274b953ea4255da5fa69e1c2
kbakoulias <kostas.bakoulias@gmail.com>
2017-03-23 15:56:52 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -3024,7 +3024,7 @@ router.post('/admin/bugs/comment', authorization, function (req, res) {
 });
 
 router.post('/admin/bugs/comment/add', authorization, function (req, res) {
-    console.log("req=====>" + JSON.parse(req));
+    console.log("req=====>" + req[0]);
 
     req.body.token = bugToken;
     request({

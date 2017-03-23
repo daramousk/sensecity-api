35652933323fb83d6cfa8d4cccdae9d62be70cb5
kbakoulias <kostas.bakoulias@gmail.com>
2017-03-23 16:20:07 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -3042,7 +3042,7 @@ router.post('/admin/bugs/comment/add', authorization, function (req, res) {
             method: "GET"
         }, function (error, response, body) {
             
-            console.log(body.bugs.alias[0]);
+            console.log(body.bugs);
             
             
 

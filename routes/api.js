62e87d4110a2cfc3ec322a5a301c94680acf6dbc
kbakoulias <kostas.bakoulias@gmail.com>
2017-03-23 11:13:53 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -90,11 +90,12 @@ request({
 //POST router
 router.post('/issue', function (req, res) {    
 
-    console.log(req);
+
     console.log(req.body.mobile_num);
     console.log(req.body.email_user);
     // Start Check The logic send email - sms mandatory
     Municipality.find({ boundaries: { $geoIntersects: { $geometry: { "type": "Point", "coordinates": [req.body.loc.coordinates[0], req.body.loc.coordinates[1]] } } } }, { "municipality": 1, "sms_key_fibair": 1, "mandatory_sms": 1, "mandatory_email": 1 }, function (req1, res1) {
+        console.log(res1[0]);
         console.log(res1[0].mandatory_email);
         console.log(res1[0].mandatory_sms);
         console.log(res1[0].sms_key_fibair);

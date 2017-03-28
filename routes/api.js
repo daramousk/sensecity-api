06c7481151aa78e3cbe7e2215ca55209cf25a230
kbakoulias <kostas.bakoulias@gmail.com>
2017-03-28 16:29:05 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -2454,7 +2454,8 @@ router.post('/is_activate_user', function (req, res) {
                 _activate_email = res8.activate;
             }
                 
-                console.log("res8===>" + res8);
+            console.log("res8===>" + res8.activate);
+            console.log("res8===>" + res8[0].activate);
                 if (req.body.mobile_num != undefined || req.body.mobile_num != '') {
                     console.log("4");
                     act_User.find({ "uuid": "web-site", "mobile_num": req.body.mobile }, { "activate_sms": 1 }, function (req9, res9) {

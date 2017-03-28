cdb3755ee020239b384dec581300a345482cc1f3
kbakoulias <kostas.bakoulias@gmail.com>
2017-03-28 16:46:58 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -2459,8 +2459,8 @@ router.post('/is_activate_user', function (req, res) {
                 act_User.find({ "uuid": "web-site", "mobile_num": req.body.mobile }, { "activate_sms": 1 }, function (req9, res9) {
                     var xxx = JSON.stringify(res9);
                     console.log(xxx);
-                    if (xxx[0] != undefined || res9[0].activate_sms != '') {
-                        //_activate_sms = res9[0].activate_sms;
+                    if (xxx != [] || res9[0].activate_sms != '') {
+                        _activate_sms = res9[0].activate_sms;
                     } else {
                         console.log("undefined");
                     }

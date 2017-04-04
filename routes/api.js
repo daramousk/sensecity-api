2cf32c51aee289371ee5dad9ff89185bc458bd8d
kbakoulias <kostas.bakoulias@gmail.com>
2017-04-04 16:40:49 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -136,7 +136,7 @@ router.post('/issue', function (req, res) {
             url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + req.body.loc.coordinates[1] + "," + req.body.loc.coordinates[0]+"&language=el&key=" + config.config.key_geocoding,
             method: "GET"
         }, function (error, response) {
-            console.log(JSON.stringify(response));
+            console.log(response);
         });
     }
 

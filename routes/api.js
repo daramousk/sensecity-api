e0d5c509b3a0a561423e932655c70f3eebbe503a
kbakoulias <kostas.bakoulias@gmail.com>
2017-04-10 12:14:55 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -87,6 +87,164 @@ request({
     }
 });
 
+router.post('/save_image', authentication, function (req, res) {
+
+
+    if (req.body.mobile_num != undefined) {
+        var _mobile_num = '';
+        var _email_user = '';
+
+        if (req.body.mobile_num != undefined) {
+            _mobile_num = req.body.mobile_num;
+        }
+
+        if (req.body.email_user != undefined) {
+            _email_user = req.body.email_user;
+        }
+
+        // Start Check The logic send email - sms mandatory
+
+        Municipality.find({ boundaries: { $geoIntersects: { $geometry: { "type": "Point", "coordinates": [req.body.loc.coordinates[0], req.body.loc.coordinates[1]] } } } }, { "municipality": 1, "sms_key_fibair": 1, "mandatory_sms": 1, "mandatory_email": 1 }, function (req1, res1) {
+            var _res1 = JSON.stringify(res1);
+
+            if (JSON.parse(_res1)[0].mandatory_email == true && _email_user == '') {
+                //Forbidden
+                res.status(403).send([{ "error_msg": "Required_email" }]);
+            }
+
+            if (JSON.parse(_res1)[0].mandatory_sms == true && _mobile_num == '') {
+                res.status(403).send([{ "error_msg": "Required_sms" }]);
+            }
+        });
+
+        // end Check The logic send email - sms mandatory
+    }
+
+    var anonymous_status = "true";
+
+    var return_var;
+    var city_name = '';
+    var city_address = '';
+
+    if (req.body.hasOwnProperty('city_address')) {
+        city_address = req.body.city_address;
+    }
+
+    if (city_address == '') {
+
+        request({
+            url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + req.body.loc.coordinates[1] + "," + req.body.loc.coordinates[0] + "&language=el&key=" + config.config.key_geocoding,
+            method: "GET"
+        }, function (error, response) {
+
+            if (JSON.parse(response.body).status == "OK") {
+                city_address = JSON.parse(response.body).results[0].formatted_address;
+            } else {
+                city_address = "N/A";
+            }
+
+            if (!req.body.hasOwnProperty('issue') ||
+                !req.body.hasOwnProperty('loc') ||
+                !req.body.hasOwnProperty('value_desc') ||
+                !req.body.hasOwnProperty('device_id')) {
+                res.statusCode = 403;
+                return res.send({ "message": "Forbidden" });
+            } else {
+
+                Municipality.find({
+                    boundaries:
+                    {
+                        $geoIntersects:
+                        {
+                            $geometry: {
+                                "type": "Point",
+                                "coordinates": req.body.loc.coordinates
+                            }
+                        }
+                    }
+                }, function (err, response) {
+
+                    var entry = new Issue({
+                        loc: { type: 'Point', coordinates: req.body.loc.coordinates },
+                        issue: req.body.issue,
+                        device_id: req.body.device_id,
+                        value_desc: req.body.value_desc,
+                        comments: req.body.comments,
+                        city_address: city_address
+                    });
+
+
+                    entry.image_name = '';
+
+                    var base64img = req.body.image_name;
+                    var base64Data = base64img.replace("data:image\png;base64,", "");
+
+
+                    console.log(base64Data);
+
+
+
+                    if (response.length > 0) {
+                        entry.municipality = response[0]["municipality"];
+
+                        city_name = response[0].municipality_desc;
+                    } else {
+                        entry.municipality = '';
+                        city_name = '';
+                    }
+                    entry.save(function (err1, resp) {
+                        if (err1) {
+                            console.log(err1);
+                        } else {
+                            if (resp.issue == "garbage" || resp.issue == "road-constructor" || resp.issue == "lighting" || resp.issue == "plumbing" || resp.issue == "protection-policy" || resp.issue == "green" || resp.issue == "environment") {
+                                if (response.length > 0) {
+
+                                    var bugData1 = { "token": bugToken, "summary": resp.issue, "priority": "normal", "bug_severity": "normal", "cf_city_name": city_name, "alias": [resp._id.toString()], "url": resp.value_desc, "product": response[0]["municipality"], "component": config.config.bug_component, "version": "unspecified", "cf_city_address": city_address };
+
+                                    request({
+                                        url: bugUrlRest + "/rest/bug",
+                                        method: "POST",
+                                        json: bugData1
+                                    }, function (error, bugResponse, body) {
+                                        console.log(JSON.stringify(bugResponse));
+                                        if (error != null) { console.log(error) };
+
+                                        if (!error && bugResponse.statusCode === 200) {
+                                            // console.log(body);
+                                        } else {
+                                            console.log("error: " + error);
+                                            console.log("bugResponse.statusCode: " + bugResponse.statusCode);
+                                            console.log("bugResponse.statusText: " + bugResponse.statusText);
+                                        }
+                                    });
+                                }
+                            }
+
+                        }
+                        return_var = { "_id": resp._id };
+                        res.send(return_var);
+                    });
+                });
+            }
+
+
+
+
+
+
+
+
+        });
+    }
+
+
+
+
+
+});
+
+
+
 //POST router
 router.post('/issue', function (req, res) {    
     if (req.body.mobile_num != undefined) {
@@ -3332,7 +3490,6 @@ router.get('/logout', authentication, function (req, res) {
         res.send("logout");
     });
 });
-
 
 function is_authenticate(req, res) {
     if (req.uuid != undefined && req.role != undefined) {

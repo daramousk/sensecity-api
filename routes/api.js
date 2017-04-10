d91c2386108a2eee79a1c986d34e40b89edd3057
kbakoulias <kostas.bakoulias@gmail.com>
2017-04-10 13:45:42 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -176,7 +176,7 @@ router.post('/save_image', function (req, res) {
 
 
                     entry.image_name = '';
-                    cosnole.log('test 1');
+                    console.log('test 1');
                     var base64img = req.body.image_name;
                     var base64Data = base64img.replace("data:image\png;base64,", "");
 

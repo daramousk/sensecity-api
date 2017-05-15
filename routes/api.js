bb32bdb6a2ad050a443cc5333ad038804dead83a
kbakoulias <kostas.bakoulias@gmail.com>
2017-05-15 14:49:21 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -962,8 +962,7 @@ router.get('/admin/issue', authentication, function (req, res) {
     req.send_severity = 1;
     req.send_priority = 1;
 
-    console.log("xxx:" + authentication);
-    console.log("00:" + req.body);
+    console.log("00:" + JSON.stringify(req.body));
 
 
 

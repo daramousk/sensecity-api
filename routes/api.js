b352dce9d00ec3e35cf2fe08dc9e966e5936f6b3
kbakoulias <kostas.bakoulias@gmail.com>
2017-05-19 14:33:17 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3810,6 +3810,7 @@ router.post('/issue_subscribe', function (req, res) {
         url: bugUrlRest + "/rest/bug" + bugParams1,
         method: "GET"
     }, function (error, response, body) {
+        console.log(JSON.stringify(body).bugs);
         if (JSON.stringify(body).bugs != undefined) {
             console.log("1");
             if (JSON.parse(body.bugs)[0].cf_email == req.body.email && JSON.parse(body.bugs)[0].cf_mobile == req.body.mobile) {

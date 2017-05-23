6165f84392f8f2b158d3e2fe048a753907c7882b
kbakoulias <kostas.bakoulias@gmail.com>
2017-05-23 15:57:07 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -962,7 +962,7 @@ router.get('/admin/issue', authentication, function (req, res) {
 
     var _city_department;
     Role.find({ "uuid": req.headers['x-uuid'], "role": req.headers['x-role'] }, { "department": 1, "city": 1 }, function (error, resp) {
-
+        console.log(resp);
         //console.log("department=>" + resp[0].department + " -- city==>" + resp[0].city);
         if (resp != undefined) {
             if (resp[0].department == '') {

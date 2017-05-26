146c97f143af0a27cb847d16b4a0ee5d57edaa39
kbakoulias <kostas.bakoulias@gmail.com>
2017-05-26 11:42:49 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3950,7 +3950,7 @@ router.post('/issue_subscribe', function (req, res) {
                             console.log(JSON.stringify(response1));
 
                             var bugComment1 = { "token": bugToken, "id": req.body.bug_id, "comment": resp.comments };
-                            console.log("1:=>" + bugComment1);
+                            console.log("1:=>" + JSON.stringify(bugComment1));
                             console.log("----------------");
                             console.log(req.body.bug_id);
 
@@ -3986,7 +3986,7 @@ router.post('/issue_subscribe', function (req, res) {
                      //add comment
                         var bugComment1 = { "token": bugToken, "id": req.body.bug_id, "comment": req.body.comment };
 
-                        console.log("2:=>" + bugComment1);
+                        console.log("2:=>" + JSON.stringify(bugComment1));
                         console.log("----------------");
                         console.log(req.body.bug_id);
                         request({

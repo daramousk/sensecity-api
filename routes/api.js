7f51fa1d099c349953f5a109c6a77ab8674e7780
kbakoulias <kostas.bakoulias@gmail.com>
2017-05-29 15:39:09 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -980,6 +980,7 @@ router.get('/admin/issue', authentication, function (req, res) {
                     _city_department_count = resp[0].departments[i].department;
                     //_city_department_count = _city_department_count.replace('&', '\/u0026');
                     _city_department += "f" + (4 + i) + "=component&o" + (4 + i) + "=equals&v" + (4 + i) + "=" + encodeURIComponent(_city_department_count);
+                    console.log("_city_department======>>>>" + _city_department);
                 }
                 _city_department += "&j3=OR&f3=OP&f" + (i + 4) + "=CP";
         } else if (resp[0].departments.length == 1 && resp[0].departments[0].department == undefined) {

86bc35499532a909b405e34610edcd7b32d23f72
kbakoulias <kostas.bakoulias@gmail.com>
2017-06-01 15:27:56 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3924,17 +3924,17 @@ router.post('/issue_subscribe', function (req, res) {
                                         if (req.body.name != undefined) {
                                             tag_name = "name:" + req.body.name;
                                         } else {
-                                            tag_name = "name:";
+                                            tag_name = "name:undefined";
                                         }
                                         if (req.body.email != undefined) {
                                             tag_email = "email:testtest.gr";// + req.body.email;
                                         } else {
-                                            tag_email = "email:";
+                                            tag_email = "email:undefined";
                                         }
                                         if (req.body.mobile_num != undefined) {
                                             tag_mobile = "mobile:" + req.body.mobile_num;
                                         } else {
-                                            tag_mobile = "mobile:";
+                                            tag_mobile = "mobile:undefined";
                                         }
                                         console.log(tag_name);
                                         console.log(tag_email);

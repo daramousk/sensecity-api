c1fedc50b1c28cbb993c915a88c7f5c60a0c17e7
kbakoulias <kostas.bakoulias@gmail.com>
2017-06-07 09:26:50 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3770,7 +3770,8 @@ router.post('/admin/bugs/comment/add', authorization, function (req, res) {
 router.post('/admin/bugs/comment/tags', authorization, function (req, res) {
     req.body.token = bugToken;
 
-    console.log(req.body);
+    console.log("1=>" + req.body.add[0]);
+    console.log("2=>" + req.body.add[1]);
 
     request({
         url: bugUrlRest + "/rest/bug/comment/" + req.body.id + "/tags",

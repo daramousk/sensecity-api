fde8fdc033ff3a6342e489c526c998a11a62d64d
kbakoulias <kostas.bakoulias@gmail.com>
2017-06-12 15:49:52 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3223,11 +3223,12 @@ router.post('/activate_user', function (req, res) {
 
                             for (var i = 0; i < 4; i++)
                                 text_act += possible.charAt(Math.floor(Math.random() * possible.length));
-
-                            act_email.update({ "_id": resp1._id}, { $set: { "email": req.query.email, "activate": text_act, } }, function (err1, resp1) {
+                            
+                            act_email.update({ "_id": resp1._id}, { $set: { "email": req.query.email, "activate": text_act, } }, function (err2, resp2) {
                                 if (err1)
                                     console.log(err1);
-                                console.log("resp1===>"+resp1);
+
+                                console.log("resp2===>"+resp2);
                                 // create reusable transporter object using the default SMTP transport 
                                 var transporter = nodemailer.createTransport('smtps://' + config.config.email + ':' + config.config.password_email + '@smtp.gmail.com');
 

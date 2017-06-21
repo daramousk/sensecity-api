c44f33cd653712bb032e4608ba33648ce580e0be
kbakoulias <kostas.bakoulias@gmail.com>
2017-06-21 16:51:33 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3146,7 +3146,7 @@ router.get('/fullissue/:id', function (req, res) {
                                 console.log("allias_issue=========>>>>>>>>" + allias_issue);
 
                                 isseu_rtn_function(allias_issue, body_var.bugs[0].id, body_var.bugs[0].cf_city_address, body_var.bugs[0].status, body1, function (callback) {
-                                    issue_rtrn.push(callback);
+                                    issue_rtrn.push(JSON.parse(callback));
                                     console.log(callback);
                                     console.log("===========>>>>>>>>>" + q + "<<<<<<<<<<======>>>>>>>>>" + (body_var.bugs.length - 1));
                                     if (q == body_var.bugs.length && callback.length > 0) {

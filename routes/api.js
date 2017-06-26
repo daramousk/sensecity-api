4d0e9ffd9f48c35e51cf162c4757841d5c5e30f1
kbakoulias <kostas.bakoulias@gmail.com>
2017-06-26 16:38:08 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -3085,6 +3085,7 @@ router.get('/city_policy', function (req, res) {
 });
 
 function return_fullissue_resp(id, alias, status, cf_city_address, callback) {
+    console.log("dzfsdfsdfsd");
     request({
         url: bugUrlRest + "/rest/bug/" + id + "/comment",
         method: "GET"

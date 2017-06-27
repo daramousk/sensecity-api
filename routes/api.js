a7ed51421e73ddea1bff9976c18faf59e35d8644
kbakoulias <kostas.bakoulias@gmail.com>
2017-06-27 10:21:42 +0300

    ok

--- routes/api.js
+++ routes/api.js
@@ -4538,7 +4538,7 @@ router.post('/issue_recommendation', function (req, res) {
             }, "loc": {
                 $near: { //$nearSphere:
                     $geometry: {
-                        type: "Point", coordinates: [req.body.long, req.body.lat]
+                        type: "Point", coordinates: [req.body.lat, req.body.long]
                     }, $minDistance: 10,
                     $maxDistance: 10
                 }
